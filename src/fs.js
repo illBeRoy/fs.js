77e4d2195f594ed79473eafe292d5f893356db4b
unknown <roy@sommer.co.il>
2014-02-14 19:49:57 +0200

    Just adding files into the git

---
+++ src/fs.js
@@ -0,0 +1,9 @@
+/*
+    The file concludes of two classes:
+        1. The FS module - which provides the interface
+        2. The FSO class - which provides an instanceable class for user input\output operations
+
+    Few notes:
+        1. You can have as many FSOs as you wish.
+        2. Unless you're adding modifications of your own, there's no real reason to directly use the FS module.
+*/
